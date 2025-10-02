a32005903a18310ab41894e96bc5ff0e3747be92
hwtb <randipa99@gmail.com>
2025-10-02 22:56:54 +1300

    Improving dev setup

--- src/dev_python/install.sh
+++ src/dev_python/install.sh
@@ -5,7 +5,12 @@ apt-get update && \
     apt-get install -y --no-install-recommends \
     curl ca-certificates
 
-curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="/usr/local/bin" sh
+if ! which uv &> /dev/null; then
+    echo "uv not found. Installing uv to /usr/local/bin..."
+    curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="/usr/local/bin" sh
+else
+    echo "uv is already installed. Skipping installation."
+fi
 
 chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.cache
 
