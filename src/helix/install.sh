68f957adf7e6ed6d3e3b5fa6198a4f96a8a375ac
hwtb <randipa99@gmail.com>
2025-10-02 14:26:07 +1300

    Installs deps if not already installed

--- src/helix/install.sh
+++ src/helix/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+if ! which rustup > /dev/null; then
+    which curl > /dev/null || (apt update && apt install curl -y -qq)
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+fi
+
 dpkg -l | grep build-essential || (apt update && apt install build-essential -y -qq)
 dpkg -l | grep pkg-config || (apt update && apt install pkg-config -y -qq)
 dpkg -l | grep git || (apt update && apt install git -y -qq)
