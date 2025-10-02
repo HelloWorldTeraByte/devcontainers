1dd3b5000570e2f6ea24f2a6e9f317c53aeccdf1
hwtb <randipa99@gmail.com>
2025-10-03 11:59:09 +1300

    Not using inline for loop

--- src/dev_python/install.sh
+++ src/dev_python/install.sh
@@ -14,4 +14,9 @@ fi
 
 chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.cache
 
-UV_TOOL_BIN_DIR=/usr/local/bin for pkg in python-lsp-server black; do uv tool install $pkg; done
+TOOLS="python-lsp-server black"
+
+for pkg in $TOOLS; do
+    echo "Installing tool: $pkg"
+    UV_TOOL_BIN_DIR=/usr/local/bin uv tool install "$pkg"
+done
