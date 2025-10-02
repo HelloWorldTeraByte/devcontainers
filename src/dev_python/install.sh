14f6167c644fc5d331a95ab00f058468f97ccb6c
hwtb <randipa99@gmail.com>
2025-10-02 17:03:06 +1300

    WIP

--- src/dev_python/install.sh
+++ src/dev_python/install.sh
@@ -9,6 +9,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.cache
 
+export UV_LINK_MODE="copy"
 export UV_TOOL_BIN_DIR="/usr/local/bin"
 uv tool install python-lsp-server
 uv tool install black
