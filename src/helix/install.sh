208c13af3dd353a71d73b77027d5a81c5ebd2c45
hwtb <randipa99@gmail.com>
2025-10-02 14:20:16 +1300

    Installs deps if not already installed

--- src/helix/install.sh
+++ src/helix/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+dpkg -l | grep build-essential || (apt update && apt install build-essential -y -qq)
+dpkg -l | grep pkg-config || (apt update && apt install pkg-config -y -qq)
+dpkg -l | grep git || (apt update && apt install git -y -qq)
+
 git clone https://github.com/HelloWorldTeraByte/dotfiles.git
 ln -s ~/dotfiles/helix/.config/helix ~/.config/helix
 
