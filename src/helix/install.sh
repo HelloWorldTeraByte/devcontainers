7633688db04750e497be2d15eeb6e93c1b82cf9d
hwtb <randipa99@gmail.com>
2025-10-02 22:32:41 +1300

    Removed deps on user as much as possible:

--- src/helix/install.sh
+++ src/helix/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+HELIX_VERSION="25.07.1"
+HELIX_ARCH="x86_64-linux"
+HELIX_DIR_NAME="helix-${HELIX_VERSION}-${HELIX_ARCH}"
+HELIX_ARCHIVE_NAME="${HELIX_DIR_NAME}.tar.xz"
+HELIX_RELEASE_URL="https://github.com/helix-editor/helix/releases/download/${HELIX_VERSION}/${HELIX_ARCHIVE_NAME}"
+
 apt-get update && \
     apt-get install -y --no-install-recommends \
     curl ca-certificates git tar xz-utils
@@ -11,25 +17,26 @@ apt-get update && \
 #     source $HOME/.cargo/env
 # fi
 
-cd $_REMOTE_USER_HOME
-git clone https://github.com/HelloWorldTeraByte/dotfiles.git
-ln -s $_REMOTE_USER_HOME/dotfiles/helix/.config/helix $_REMOTE_USER_HOME/.config/helix
+git clone https://github.com/HelloWorldTeraByte/dotfiles.git /opt/dotfiles
+
+mkdir -p $_REMOTE_USER_HOME/.config
+ln -s /opt/dotfiles/helix/.config/helix $_REMOTE_USER_HOME/.config/helix
 
 cd /tmp
-curl -LO https://github.com/helix-editor/helix/releases/download/25.07.1/helix-25.07.1-x86_64-linux.tar.xz
+curl -LO $HELIX_RELEASE_URL
 
 cd /opt
-tar -xf /tmp/helix-25.07.1-x86_64-linux.tar.xz
-rm  /tmp/helix-25.07.1-x86_64-linux.tar.xz
-mv helix-25.07.1-x86_64-linux helix
+tar -xf /tmp/$HELIX_ARCHIVE_NAME
+rm  /tmp/$HELIX_ARCHIVE_NAME
+mv $HELIX_DIR_NAME helix
 chmod +x helix/hx
 ln -s /opt/helix/hx /usr/local/bin
 
-# git clone https://github.com/helix-editor/helix programs/helix
-# cd programs/helix
-# git checkout 25.07.1
+# git clone https://github.com/helix-editor/helix /opt/helix
+# cd /opt/helix
+# git checkout $HELIX_VERSION
 # cargo build --release
-# ln -s $_REMOTE_USER_HOME/programs/helix/target/release/hx $_REMOTE_USER_HOME/.local/bin/
+# ln -s /opt/helix/target/release/hx /usr/local/bin
 
 ln -Tsf /opt/helix/runtime $_REMOTE_USER_HOME/.config/helix/runtime
 
